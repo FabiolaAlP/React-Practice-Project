d7aeb796c5c2705d63b41a63d142dc62d920c844
Fabiola Alvarado <fabiolaalvarado11@gmail.com>
2023-05-08 17:47:48 -0600

    adding user, userlist and button components and their style

---
+++ src/components/Users/AddUsers.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+import Button from "../UI/Button";
+const AddUsers = (props) => {
+  const [user, setUser] = useState("");
+  const [age, setAge] = useState("");
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    if (user.trim().length === 0 || age.trim().length === 0) {
+      return;
+    }
+    if (+age < 1) {
+      return;
+    }
+    console.log(user, age);
+    setUser("");
+    setAge("");
+  };
+  const usernameChangeHandler = (event) => {
+    setUser(event.target.value);
+  };
+  const ageChangeHandler = (event) => {
+    setAge(event.target.value);
+  };
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          type="text"
+          value={user}
+          onChange={usernameChangeHandler}
+        />
+        <label htmlFor="age">Age (years)</label>
+        <input id="age" type="number" value={age} onChange={ageChangeHandler} />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUsers;
